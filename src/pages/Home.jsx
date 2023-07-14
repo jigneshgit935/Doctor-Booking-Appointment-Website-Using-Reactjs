9c087d3e54674dc45fafd88510b97e8b2eb8372c
Jignesh <jignesh.sharma.707@gmail.com>
2023-07-15 03:08:09 +0530

    feature-section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ import heroImg03 from '../assets/images/hero-img03.png';
 import icon01 from '../assets/images/icon01.png';
 import icon02 from '../assets/images/icon02.png';
 import icon03 from '../assets/images/icon03.png';
+import featureImg from '../assets/images/feature-img.png';
+import videoIcon from '../assets/images/video-icon.png';
+import avatarIcon from '../assets/images/avatar-icon.png';
 import { BsArrowRight } from 'react-icons/bs';
 import About from '../components/About/About';
 import ServiceList from '../components/Services/ServiceList';
@@ -157,17 +160,79 @@ const Home = () => {
       <About />
 
       {/* services section */}
-      <div className="container">
-        <div className="xl:w-[470px] mx-auto">
-          <h2 className="heading text-center">Our Medical Services</h2>
-          <p className="text__para text-center">
-            World-class care for everyone. Our health System offers unmatched,
-            expert health care.
-          </p>
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">Our Medical Services</h2>
+            <p className="text__para text-center">
+              World-class care for everyone. Our health System offers unmatched,
+              expert health care.
+            </p>
+          </div>
+          <ServiceList />
         </div>
-        <ServiceList />
-      </div>
+      </section>
       {/* services section */}
+
+      {/* feature section */}
+      <section>
+        <div className="container">
+          <div className="flex items-center justify-between flex-col lg:flex-row ">
+            {/* feature content */}
+            <div className="xl:w-[670px] ">
+              <h2 className="heading ">
+                Get virtual treatment <br /> anytime.
+              </h2>
+
+              <ul className="pt-4">
+                <li className="text__para">
+                  1. Schedule the appointment directly
+                </li>
+                <li className="text__para">
+                  2. Search for your physician here, and contact their office.
+                </li>
+                <li className="text__para">
+                  3. View our physician who are accepting new patients, use the
+                  online scheduling tool to select an appointment time.
+                </li>
+              </ul>
+              <Link to="/">
+                <button className="btn">Learn More</button>
+              </Link>
+            </div>
+            {/* featuer img */}
+
+            <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
+              <img src={featureImg} alt="" />
+              <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4  lg:pb-[26px] rounded-[10px]">
+                <div className="flex items-center justify-between ">
+                  <div className="flex items-center gap-[6px] lg:gap-3">
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]">
+                      Tue, 24
+                    </p>
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400]">
+                      10:00
+                    </p>
+                  </div>
+                  <span className="w-5 h-5 lg:w-[34px]  lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]">
+                    <img src={videoIcon} alt="" />
+                  </span>
+                </div>
+                <div className="w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full">
+                  Consultation
+                </div>
+                <div className="flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]">
+                  <img src={avatarIcon} alt="" />
+                  <h1 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor">
+                    Wayne Collins
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* feature section */}
     </>
   );
 };
